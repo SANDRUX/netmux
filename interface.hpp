a5fa03d3a6c1f6f21f41518f3f0ace7c24deb6b1
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-05-15 00:01:04 +0400

    TcpListener and TcpClient classes have been added

--- interface.hpp
+++ interface.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <stdint.h>
 #include <unistd.h>
@@ -11,27 +12,33 @@
 
 namespace netmux
 {
-    enum {LISTENER, CLIENT};
-
     class socket
     {
     protected:
         int sfd;
 
     public:
-        virtual bool send(const std::vector<uint8_t> &) = 0;
-        virtual bool receive(std::vector<uint8_t> &) = 0;
-        virtual ~socket() = 0;
+        virtual size_t send (const std::vector<uint8_t> &) const = 0;
+        virtual size_t receive (std::vector<uint8_t> &) const = 0;
+        virtual ~socket () = 0;
     };
 
-    class TcpSocket final : public socket
+    class TcpClient final : public socket
     {
     public:
-        TcpSocket (const int);
-        TcpSocket (const TcpSocket &);
-        bool operator=(const TcpSocket &);
-        bool send(const std::vector<uint8_t> &) const;
-        bool receive(std::vector<uint8_t> &) const;
-        ~TcpSocket();
+        TcpClient ();
+        TcpClient (const TcpClient &);
+        void operator = (const TcpClient &);
+        bool connect (const std::string &, const int) const;
+        size_t send (const std::vector<uint8_t> &) const override;
+        size_t receive (std::vector<uint8_t> &) const override;
+        ~TcpClient ();
+    };
+
+    class TcpListener final : public socket
+    {
+        TcpListener ();
+        TcpListener (const TcpListener &);
+        bool operator = (const TcpListener &);
     };
 }
